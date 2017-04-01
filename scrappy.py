eccd3aefb95b37fab42b76f74c59dd758dcd3371
Jhonatan Cruz <jcruz3@drew.edu>
2017-04-01 17:34:28 -0400

    now has chrome driver

--- scrappy.py
+++ scrappy.py
@@ -1,6 +1,10 @@
 from bs4 import BeautifulSoup
 from flask import Flask, render_template, request
 import urllib.request
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
 
 app= Flask(__name__)
 
@@ -10,11 +14,13 @@ def index():
 
 @app.route('/url', methods=['GET', 'POST'])
 def evaluateUrl():
+    global url
     url= request.form['URL']
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as response:
        the_page = response.read()
 
+    global soup
     soup = BeautifulSoup(the_page, "lxml")
     webTitle= soup.title
 
@@ -25,7 +31,25 @@ def evaluateUrl():
         else:
             print("dont enter ")
 
-    return render_template("urlEval.html", urlRemove=urlRemove)
+    return render_template("urlEval.html", urlRemove=urlRemove, url= url)
+
+@app.route('/getLines', methods=['GET', 'POST'])
+def getLines():
+    driver= webdriver.Chrome()
+    driver.set_window_size(1124, 850) # set browser size.
+    driver.get(url)
+
+    time.sleep(1)
+    moreButton =driver.find_element_by_xpath("//*[@id='action-panel-overflow-button']/span").click()
+    # moreButton =driver.find_element_by_xpath("//*[@id='action-panel-overflow-button']/span").click()
+
+    transcriptButton= driver.find_element_by_xpath("//*[@id='action-panel-overflow-menu']/li[2]/button/span").click()
+    time.sleep(1)
+    firstLine=soup.find("div", {"id": "cp-0"})
+
+    print(firstLine)
+
+    return render_template("lineValues.html", firstLine=firstLine)
 
 if __name__=="__main__":
     app.run(debug=True)
