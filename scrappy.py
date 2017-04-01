b227effe6727ecb138ca6bf56fbda606e19dc116
Jhonatan Cruz <jcruz3@drew.edu>
2017-04-01 15:48:02 -0400

    added an url evaluation

--- scrappy.py
+++ scrappy.py
@@ -1,20 +1,24 @@
 from bs4 import BeautifulSoup
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import urllib.request
 
 app= Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    req = urllib.request.Request('https://www.youtube.com/')
+    return render_template("popup.html")
+
+@app.route('/', methods=['GET', 'POST'])
+def evaluativeUrl():
+    url= request.form['URL']
+    req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as response:
        the_page = response.read()
 
     soup = BeautifulSoup(the_page, "lxml")
     webTitle= soup.title
-    # print(soup.title)
 
-    return render_template("popup.html", webTitle= webTitle)
+    return render_template("urlEval.html", webTitle=webTitle)
 
 if __name__=="__main__":
     app.run(debug=True)
